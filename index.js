fd8c0192afe0cd36411a58623b5430493d59c81e
Veekshith-Thoram <67872770+Veekshith-Thoram@users.noreply.github.com>
2025-01-12 06:52:38 -0600

    uncommenting previous code

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-// // console.log("Hey Sadhvika!!");
-// // console.log("I love you thalli");
-// // console.log("Thank You for Coming into my life!!")
-// // for loop intro
-// for(let i = 0; i < 5; i++){
-//     console.log("SadhVee")
-// }
+console.log("Hey Sadhvika!!");
+console.log("I love you thalli");
+console.log("Thank You for Coming into my life!!")
+// for loop intro
+for(let i = 0; i < 5; i++){
+    console.log("SadhVee")
+}
 
-// //arrays
-// var groceryList = ["milk", "water", "fruits", "medicine", "liquor"];
-// console.log(groceryList[0])
-// console.log(groceryList[2])
-// console.log(groceryList[4])
+//arrays
+var groceryList = ["milk", "water", "fruits", "medicine", "liquor"];
+console.log(groceryList[0])
+console.log(groceryList[2])
+console.log(groceryList[4])
 
-// // .length property
-// var lengthOfGroceryList = groceryList.length;
-// console.log(lengthOfGroceryList);
-// // console.log(groceryList[lengthOfGroceryList - 1]);
-// function log(data){
-//     console.log(data)
-// }
-// //for and array clubbing
-// for ( let i = 0; i < lengthOfGroceryList; i++) {
-//     log(groceryList[i]);
-// }
+// .length property
+var lengthOfGroceryList = groceryList.length;
+console.log(lengthOfGroceryList);
+// console.log(groceryList[lengthOfGroceryList - 1]);
+function log(data){
+    console.log(data)
+}
+//for and array clubbing
+for ( let i = 0; i < lengthOfGroceryList; i++) {
+    log(groceryList[i]);
+}
 
 //objects and array of objects
 
