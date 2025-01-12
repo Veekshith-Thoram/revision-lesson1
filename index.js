bf8f236ad0474f36c15fd89560b00b1a525997e4
Veekshith-Thoram <67872770+Veekshith-Thoram@users.noreply.github.com>
2025-01-12 02:54:24 -0600

    for loop and array intro

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-console.log("Hey Sadhvika!!");
-console.log("I love you thalli");
-console.log("Thank You for Coming into my life!!")
+// console.log("Hey Sadhvika!!");
+// console.log("I love you thalli");
+// console.log("Thank You for Coming into my life!!")
+// for loop intro
+for(let i = 0; i < 5; i++){
+    console.log("SadhVee")
+}
+
+//arrays
+var groceryList = ["milk", "water", "fruits", "medicine", "liquor"];
+console.log(groceryList[0])
+console.log(groceryList[2])
+console.log(groceryList[4])
+
+// .length property
+var lengthOfGroceryList = groceryList.length;
+console.log(lengthOfGroceryList);
+console.log(groceryList[lengthOfGroceryList - 1]);
